ff51643c1ea8d2a1f2ef6dd31c270d57a200ded3
tpinto7 <tjpinto@uwaterloo.ca>
2020-02-01 12:34:08 -0500

    set up map

--- fireguard/src/App.vue
+++ fireguard/src/App.vue
@@ -1,18 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <GmapMap :center="{ lat: 10, lng: 10 }"> -->
+  <!-- <GmapMarker
+    v-for="(m, index) in markers"
+    :key="index"
+    :position="m.position"
+    @click="center = m.position"
+  /> -->
+   <google-map />
+  Adiiiiiis
+<!-- </GmapMap> -->
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import GoogleMap from "./components/GoogleMap";
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    GoogleMap
+  },
+  // async mounted(){
+  //     try {
+  //     const google = await gmapsInit();
+  //     const geocoder = new google.maps.Geocoder();
+  //     const map = new google.maps.Map(this.$el);
+
+  //     geocoder.geocode({ address: 'Austria' }, (results, status) => {
+  //       if (status !== 'OK' || !results[0]) {
+  //         throw new Error(status);
+  //       }
+
+  //       map.setCenter(results[0].geometry.location);
+  //       map.fitBounds(results[0].geometry.viewport);
+  //     });
+  //   } catch (error) {
+  //    // console.error(error);
+  //   }
+  // }
 }
 </script>
 
--- fireguard/src/main.js
+++ fireguard/src/main.js
@@ -1,7 +1,44 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from "vue2-google-maps";
 
 Vue.config.productionTip = false
+
+// var heatmapData = [
+//   new google.maps.LatLng(37.782, -122.447),
+//   new google.maps.LatLng(37.782, -122.445),
+//   new google.maps.LatLng(37.782, -122.443),
+//   new google.maps.LatLng(37.782, -122.441),
+//   new google.maps.LatLng(37.782, -122.439),
+//   new google.maps.LatLng(37.782, -122.437),
+//   new google.maps.LatLng(37.782, -122.435),
+//   new google.maps.LatLng(37.785, -122.447),
+//   new google.maps.LatLng(37.785, -122.445),
+//   new google.maps.LatLng(37.785, -122.443),
+//   new google.maps.LatLng(37.785, -122.441),
+//   new google.maps.LatLng(37.785, -122.439),
+//   new google.maps.LatLng(37.785, -122.437),
+//   new google.maps.LatLng(37.785, -122.435)
+// ];
+
+// var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+
+// map = new google.maps.Map(document.getElementById('map'), {
+//   center: sanFrancisco,
+//   zoom: 13,
+//   mapTypeId: 'satellite'
+// });
+
+// var heatmap = new google.maps.visualization.HeatmapLayer({
+//   data: heatmapData
+// });
+// heatmap.setMap(map);
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyAqi3uNZU9jTFavql1RDAhVOw6H0MaO6bs",
+    libraries: "places" // necessary for places input
+  }
+});
 
 new Vue({
   render: h => h(App),
